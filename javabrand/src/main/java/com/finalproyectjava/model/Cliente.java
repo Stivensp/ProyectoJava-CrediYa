da3ac96cb508986f5cc4f686438a20edb948f9c2
Stiven <delleore1@gmail.com>
2025-12-11 12:02:40 -0500

    feat(UI) :sparkles: Creacion del modelo Cliente

---
+++ javabrand/src/main/java/com/finalproyectjava/model/Cliente.java
@@ -0,0 +1,28 @@
+package com.finalproyectjava.model;
+
+public class Cliente extends Persona {
+    //Atibutos
+    private String telefono;
+
+    //Constructor
+    public Cliente (int id, String nombre, String documento, String correo, String telefono){
+        super(id, nombre, documento, correo);
+        this.telefono = telefono;
+    }
+
+    //Setters
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+    //Getters
+    public String getTelefono(){
+        return this.telefono;
+    }
+    
+    //toString
+    @Override
+    public String toString(){
+        return super.toString() +
+        " telefono " + telefono;
+    }
+}
