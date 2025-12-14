2dc252315c275234cfaf9e6cf88dd88cf2120fe4
Brandon <delleore1@gmail.com>
2025-12-14 16:15:39 -0500

    feat(UI) :sparkles: Empleados listos para enviar a DAO

--- javabrand/src/main/java/com/finalproyectjava/service/EmpleadoService.java
+++ javabrand/src/main/java/com/finalproyectjava/service/EmpleadoService.java
@@ -1,5 +1,59 @@
 package com.finalproyectjava.service;
 
+import com.finalproyectjava.model.Empleado;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class EmpleadoService {
-    
+    //Atributos
+    private final List<Empleado> empleados = new ArrayList<>();
+    private int countIds = 1;
+    //Metodo Registrar
+    public Empleado registrarEmpleado(String nombre, String documento, String rol, String correo, Double salario){
+        Empleado empleado = new Empleado(
+            countIds++,
+            nombre,
+            documento,
+            rol,
+            correo,
+            salario
+        );
+        empleados.add(empleado);
+        return empleado;
+    }
+    //Lista de todos los empleados
+    public List<Empleado> listaEmpleados(){
+        return empleados;
+    }
+    //Buscar por id empleados
+    public Empleado buscarEmpleadoId(int id){
+        for(Empleado e : empleados){
+            if(e.getId() == id){
+                return e;
+            }
+        }
+        return null;
+    }
+    //Actualizar empleado
+    public Empleado actualizarEmpleado(int id, String nombre, String documento, String rol, String correo, Double salario){
+        Empleado e = buscarEmpleadoId(id);
+        if(e != null){
+            e.setNombre(nombre);
+            e.setDocumento(documento);
+            e.setRol(rol);
+            e.setCorreo(correo);
+            e.setSalario(salario);
+        }
+        return e;
+    }
+    //Eliminar Empleado
+    public Boolean eliminarEmpleado(int id){
+        Empleado e = buscarEmpleadoId(id);
+        if(e != null){
+            empleados.remove(e);
+            return true;
+        }
+        return false;
+    }
 }
--- javabrand/src/main/java/com/finalproyectjava/util/view/EmpleadoView.java
+++ javabrand/src/main/java/com/finalproyectjava/util/view/EmpleadoView.java
@@ -1,9 +1,13 @@
 package com.finalproyectjava.util.view;
 
+import com.finalproyectjava.model.Empleado;
+import com.finalproyectjava.service.EmpleadoService;
+
 public class EmpleadoView extends MenuBaseView {
+    private final EmpleadoService e;
     //Constructor vacio
     public EmpleadoView(){
-    //Vacio
+        this.e = new EmpleadoService();
     }
     //Base del menu
     @Override
@@ -22,13 +26,13 @@ public class EmpleadoView extends MenuBaseView {
             ▀███████ ██ ██ ██ ████▀ ██ ▀█▄▄▄ ▀█▄██ ▀████ ▀███▀ ▄▄▄█▀ 
                             ██                                     
                             ▀▀                                               
-             0. Registrar empleado
-             0. Listar empleados
-             0. Buscar empleado por ID
-             0. Actualizar empleado
-             0. Eliminar empleado
-             0. Registrar empleado en BD (MySQL)
-             0. Listar empleados desde BD
+             1. Registrar empleado
+             2. Listar empleados
+             3. Buscar empleado por ID
+             4. Actualizar empleado
+             5. Eliminar empleado
+             6. Registrar empleado en BD (MySQL)
+             7. Listar empleados desde BD
              0. Volver
              Seleccione una opcion:   
             """
@@ -41,34 +45,102 @@ public class EmpleadoView extends MenuBaseView {
 
             opcion = consola.nextInt();
 
-            if(opcion < 0 || opcion > 5 ){
+            if(opcion < 0 || opcion > 7 ){
                 System.out.println("Opción fuera de rango");
                 continue;
             }
 
             switch (opcion) {
-                case 1:
+                case 1 -> registrarEmpleadoView();
+                case 2 -> {
                     limpiarConsola();
-                    break;
-                case 2:
-                    limpiarConsola();
-                    break;
-                case 3:
-                    limpiarConsola();                    
-                    break;
-                case 4:
-                    limpiarConsola();                    
-                    break;
-                case 5:
-                    limpiarConsola();                    
-                    break;
-                case 0:
-                    limpiarConsola();                    
-                    break;
+                    listaEmpleadosView();
+                }
+                case 3 -> buscaIdEmpleadosView();
+                case 4 -> actualizarEmpleadoView();
+                case 5 -> eliminarEmpleadoIdView();
+                case 0 -> limpiarConsola();
             
-                default: 
-                    break;
+                default -> {
+                }
             }
         } while(opcion != 0);
+    }
+
+    //Metodo de registro
+    public void registrarEmpleadoView(){
+        System.out.println("Nombre ");
+        String nombre = consola.next();
+
+        System.out.println("Documento ");
+        String documento = consola.next();
+
+        System.out.println("Rol" );
+        String rol = consola.next();
+
+        System.out.println("Correo ");
+        String correo = consola.next();
+
+        System.out.println("Salario" );
+        Double salario = consola.nextDouble();
+
+        e.registrarEmpleado(nombre, documento, rol, correo, salario);
+    }
+
+    //Metodo de listar empleados
+    public void listaEmpleadosView(){
+        for(Empleado em : e.listaEmpleados()){
+            System.out.println(em);
+        }
+    }
+    //Metodo buscar por id
+    public void buscaIdEmpleadosView(){
+        System.out.println("Buscador de ID ");
+        int findId = consola.nextInt();
+
+        Empleado em = e.buscarEmpleadoId(findId);
+
+        if(em != null){
+            System.out.println(em);
+            System.out.println("Empleado encontrado");
+        }else{
+            System.out.println("ID no encontrado");
+        }
+    }
+
+    //Metodo actualizar empleado
+    public void actualizarEmpleadoView(){
+        System.out.println("Buscador de ID ");
+        int id = consola.nextInt();
+
+        System.out.println("Nombre ");
+        String nombre = consola.next();
+
+        System.out.println("Documento ");
+        String documento = consola.next();
+
+        System.out.println("Rol" );
+        String rol = consola.next();
+
+        System.out.println("Correo ");
+        String correo = consola.next();
+
+        System.out.println("Salario" );
+        Double salario = consola.nextDouble();
+
+        e.actualizarEmpleado(id, nombre, documento, rol, correo, salario);
+    }
+
+    //Metodo eliminar empleado
+    public void eliminarEmpleadoIdView(){
+        System.out.println("Buscador de ID ");
+        int findId = consola.nextInt();
+        
+        boolean siOno = e.eliminarEmpleado(findId);
+        if(siOno == true){
+            System.out.println("Empleado eliminado");
+        }else{
+            System.out.println("Empleado no encontrado");
+        }
     }
 }
