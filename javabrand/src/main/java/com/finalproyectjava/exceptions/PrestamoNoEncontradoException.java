04c45b6be7de10d45430fb74de0f39f8b314904b
Brandon <delleore1@gmail.com>
2025-12-15 06:53:44 -0500

    feat(UI) :sparkles: Actualizacion de exeptions

---
+++ javabrand/src/main/java/com/finalproyectjava/exceptions/PrestamoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.finalproyectjava.exceptions;
+
+public class PrestamoNoEncontradoException extends Exception {
+    public PrestamoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ javabrand/src/main/java/com/finalproyectjava/exceptions/ValidacionException.java
@@ -0,0 +1,7 @@
+package com.finalproyectjava.exceptions;
+
+public class ValidacionException extends Exception {
+    public ValidacionException(String mensaje) {
+        super(mensaje);
+    }
+}
